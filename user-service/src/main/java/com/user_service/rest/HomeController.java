44783e6930cd6be26b4af2524d63704ccde4e37f
Amiel De Los Reyes <amielxvr@gmail.com>
2024-06-22 15:47:40 +0800

    Initial user-service commit

---
+++ user-service/src/main/java/com/user_service/rest/HomeController.java
@@ -0,0 +1,13 @@
+package com.user_service.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String hello() {
+        return "Hello User Service!";
+    }
+}
