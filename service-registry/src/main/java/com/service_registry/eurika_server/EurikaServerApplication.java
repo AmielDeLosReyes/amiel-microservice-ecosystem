11adaaa14394ab3f1843247a58e55ebf3211abc5
Amiel De Los Reyes <amielxvr@gmail.com>
2024-06-22 12:09:14 +0800

    Initial setup with service-registry project

---
+++ service-registry/src/main/java/com/service_registry/eurika_server/EurikaServerApplication.java
@@ -0,0 +1,13 @@
+package com.service_registry.eurika_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurikaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurikaServerApplication.class, args);
+	}
+
+}
